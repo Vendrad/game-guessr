d81e52f78ce48745bf292a2bc1991ac9d4957d3c
Dane Shenton <dane.shenton@gmail.com>
2019-04-08 23:49:12 +0200

    Finalized GameOver using memo to stop rerenders.

--- src/components/GameOver/GameOver.js
+++ src/components/GameOver/GameOver.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React from 'react';
 import { PropTypes } from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
@@ -9,21 +9,17 @@ import GameQuip from './GameQuip/GameQuip';
 
 const GameOver = (props) => {
 
-  console.log("gameover");
-
   const playAgainButtonWasClickedHandler = () => {
     props.history.push('/new-game');
   };
-
-  const count = useMemo(() => props.correctCount);
 
   return (
     <Modal
       modalExitedCallback={props.answerWasDisplayed}
       automaticallyExit={false}
       styles={styles.GameOverModal}>
-      <p className={styles.FinalScore}>You scored: {count}</p>
-      <GameQuip correctCount={count} />
+      <p className={styles.FinalScore}>You scored: {props.correctCount}</p>
+      <GameQuip correctCount={props.correctCount} />
       <p className={styles.Plug}>Head over to <a href="https://igdb.com">igdb.com</a> to discover more games!</p>
       <button className={styles.PlayAgainButton} onClick={playAgainButtonWasClickedHandler}>Play Again</button>
     </Modal>
--- src/components/GameOver/GameQuip/GameQuip.js
+++ src/components/GameOver/GameQuip/GameQuip.js
@@ -1,57 +1,53 @@
-// import React, {useEffect} from 'react';
-// import { PropTypes } from 'prop-types';
+import React, { memo } from 'react';
+import { PropTypes } from 'prop-types';
 
-// import { randBetweenInclusive } from '../../../helpers';
+import { randBetweenInclusive } from '../../../helpers';
 
-// import styles from './GameQuip.module.scss';
+import styles from './GameQuip.module.scss';
 
-const GameQuip = () => {
+const GameQuip = props => {
 
-  console.log("here");
+  const badResult = [
+    "/ff",
+    "Game Over. Insert coin to continue.",
+    "bg",
+    "Must've been the lag, right?",
+    "Mama mia...",
+    "You were not prepared.",
+    "Do you always need to be carried this hard?",
+    "You call yourself a gamer?",
+    "l2p noob",
+    "There is no aggro reset...",
+    "That's a 50dkp minus!",
+    "Game over, man... Game over...",
+    "The lights are on but nobody is home.",
+  ];
 
-  return null;
+  const goodResult = [
+    "omg wtf lag",
+    "Your opponent has disconnected...",
+    "You have received a friends request from GG.",
+    "I bet you've completed your steam collection.",
+    "M-M-M-Monster Kill!",
+    "C-C-C-Combo Breaker!",
+    "gg",
+    "grats",
+    "A WINNER IS YOU",
+    "Parzival wished he had your skills."
 
-  // const badResult = [
-  //   "/ff",
-  //   "Game Over: Insert Coin",
-  //   "bg",
-  //   "Must've been the lag, right?",
-  //   "Mama mia...",
-  //   "You were not prepared.",
-  //   "Do you always need to be carried this hard?",
-  //   "You call yourself a gamer?",
-  //   "l2p noob",
-  //   "There is no aggro reset...",
-  //   "That's a 50dkp minus!",
-  //   "Game over, man... Game over...",
-  //   "The lights are on but nobody is home.",
-  // ];
+  ];
 
-  // const goodResult = [
-  //   "omg wtf lag",
-  //   "Your opponent has disconnected...",
-  //   "You have received a friends request from GG.",
-  //   "I bet you've completed your steam collection.",
-  //   "M-M-M-Monster Kill!",
-  //   "C-C-C-Combo Breaker!",
-  //   "gg",
-  //   "grats",
-  //   "A WINNER IS YOU",
-  //   "Parzival wished he had your skills."
+  const quip = props.correctCount >= 5
+    ? goodResult[randBetweenInclusive(0, goodResult.length)]
+    : badResult[randBetweenInclusive(0, goodResult.length)];
 
-  // ];
-
-  // const quip = props.correctCount >= 5
-  //   ? goodResult[randBetweenInclusive(0, goodResult.length)]
-  //   : badResult[randBetweenInclusive(0, goodResult.length)];
-
-  // return (
-  //   <p className={styles.GameQuip}>"{quip}"</p>
-  // );
+  return (
+    <p className={styles.GameQuip}>"{quip}"</p>
+  );
 }
 
-// GameQuip.propTypes = {
-//   correctCount: PropTypes.number
-// }
+GameQuip.propTypes = {
+  correctCount: PropTypes.number
+}
 
-export default GameQuip;
+export default memo(GameQuip);

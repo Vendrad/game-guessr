f3ffcf82fe293c0be0c12465b6fd50c964062a64
Dane Shenton <dane.shenton@gmail.com>
2019-04-11 23:45:35 +0200

    GameMode tests.

---
+++ src/components/GameMode/GameMode.test.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockCR } from '../../helpers/testing';
+
+import GameMode from './GameMode';
+
+describe('<GameMode />', () => {
+
+  let defaultProps, wrapper, spy;
+
+  beforeEach(() => {
+
+    defaultProps = {
+      gameMode: {id: 0, type: 'normal', title: '80s', slug: '80', apiSlug: '0'},
+      gameModeWasSelected: jest.fn()
+    }
+
+    wrapper = shallow(<GameMode {...defaultProps} />);
+  });
+
+  afterEach(() => {
+    spy !== undefined 
+      && mockCR(spy);
+  });
+
+  it('should render without errors.', () => {
+    expect(wrapper.find('.GameMode')).toHaveLength(1);
+  });
+
+  it('should render with the given title.', () => {
+    expect(wrapper.find('p').getElement().props.children).toBe(defaultProps.gameMode.title);
+  });
+
+  it('should fire the callback when it is clicked.', () => {
+    wrapper.find('.GameMode').simulate('click');
+    expect(defaultProps.gameModeWasSelected).toHaveBeenCalledWith(defaultProps.gameMode.id);
+  });
+
+});

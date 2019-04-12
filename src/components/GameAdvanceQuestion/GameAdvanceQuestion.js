23894f69cb56045636062d7b68a1851baa442374
Dane Shenton <dane.shenton@gmail.com>
2019-04-12 11:40:51 +0200

    Testing lint-staged

--- src/components/GameAdvanceQuestion/GameAdvanceQuestion.js
+++ src/components/GameAdvanceQuestion/GameAdvanceQuestion.js
@@ -9,7 +9,7 @@ import styles from './GameAdvanceQuestion.module.scss';
 /**
  * General wrapper for the components to advance to the next question
  */
-const GameAdvanceQuestion = ({disabled, answerWasSubmitted}) => {
+const GameAdvanceQuestion = ({ disabled, answerWasSubmitted }) => {
   return (
     <div className={styles.GameAdvanceQuestion}>
       <GameSkip
